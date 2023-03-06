049ac2c18807a40cc8bc273cc71e261a04588b51
Maria Lomeli <lomeli.aiengineer@gmail.com>
2023-03-06 19:13:26 -0400

    deleting ngrok

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const { google } = require('googleapis');
 const moment = require('moment-timezone');
 const express = require('express');
 const app = express();
-const ngrok = require('ngrok');
+//const ngrok = require('ngrok');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.post('/saveDate', (req, res) => {
     console.log(req.body);
-    let {serviceAccount, timezone, data, sheet, spreadsheetId} = req.body
+    let { serviceAccount, timezone, data, sheet, spreadsheetId } = req.body
     saveDataSheet(serviceAccount, timezone, data, sheet, spreadsheetId, resp => {
         res.send(resp);
     })
-    
+
 })
 
 function saveDataSheet(serviceAccount, timezone = 'Asia/Dubai', data, sheet, spreadsheetId, callbackFunction) {
@@ -23,7 +23,7 @@ function saveDataSheet(serviceAccount, timezone = 'Asia/Dubai', data, sheet, spr
         key: serviceAccount.private_key,
         scopes: 'https://www.googleapis.com/auth/spreadsheets'
     });
-    
+
     const sheets = google.sheets('v4');
 
     console.log('save data in google sheet')
@@ -55,7 +55,7 @@ function saveDataSheet(serviceAccount, timezone = 'Asia/Dubai', data, sheet, spr
 }
 
 app.listen(port, async () => {
-    const url = await ngrok.connect(port);
-    console.log(`Server running on ${url}`);
-  
-  })
+    //const url = await ngrok.connect(port);
+    console.log(`Server running`);
+
+})
